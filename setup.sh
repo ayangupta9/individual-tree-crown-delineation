a14832e19a3d733fc5fc9f2bb35b0e07a1bc3348
Ayan Gupta <ayangupta1000@gmail.com>
2024-02-22 23:45:55 +0100

    pointrend training

--- setup.sh
+++
@@ -1,9 +0,0 @@
-conda create --name itcd_env python=3.8
-conda activate itcd_env
-
-conda install nvidia::cuda
-conda install nvidia::cuda-toolkit
-
-python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-
-pip install -r requirements.txt
